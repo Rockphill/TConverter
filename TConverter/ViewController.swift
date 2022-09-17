01c59bb662034c738d47774b2c7cec036470c2f8
Andrei <macwin33@gmail.com>
2022-09-17 21:22:28 +0300

    add -temperature

--- TConverter/ViewController.swift
+++ TConverter/ViewController.swift
@@ -9,11 +9,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var celciusLabel: UILabel!
+    @IBOutlet weak var fahrenheitLabal: UILabel!
+    @IBOutlet weak var slider: UISlider! {
+        didSet {
+            slider.maximumValue = 100
+            slider.minimumValue = -100
+            slider.value = 0
+            
+        }
     }
-
-
+    
+    @IBAction func sliderChange(_ sender: UISlider) {
+        let temperatureCelsius = Int(round(sender.value))
+        celciusLabel.text = "\(temperatureCelsius)ºC"
+        let fahrenheitTemperature = Int(round((sender.value * 9 / 5) + 32))
+        fahrenheitLabal.text = "\(fahrenheitTemperature)ºF"
+    }
 }
 
